ca9f22b1828a7236b01f850abc1d4aaf2946eef3
unknown <maryam.pourganji92@gmail.com>
2022-07-26 15:28:20 +0430

    fix file structure and set routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
+import React from "react";
 import './index.css';
-import MainPage from './layouts/mainPage';
-import SignIn from './layouts/signIn';
-import SignUp from './layouts/signUp';
-import {Route,Switch} from 'react-router-dom';
+import MainPage from './layouts/MainPage';
+import SignIn from './layouts/SignIn';
+import SignUp from './layouts/SignUp';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-  
-    <>
-     <p className='bg-red-500'>Bookshop Ecommerce</p>
-    </>
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/" element={<MainPage />} />
+    </Routes>
   )
 }
 
