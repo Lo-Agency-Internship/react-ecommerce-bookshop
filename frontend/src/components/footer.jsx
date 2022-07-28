34066d4b9449ad0a600eb109704df0ecde538326
samira kian <103483684+samikia@users.noreply.github.com>
2022-07-28 10:10:59 +0430

    change the name of files

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -6,7 +6,7 @@ import {Icons} from "./menus";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white mt-80">
-    
+  
       <ItemsContainer />
       <div
         className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-20
