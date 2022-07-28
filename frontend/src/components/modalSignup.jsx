7586697c2bb927ef82c579bcf78fed841a4f39d1
unknown <maryam.pourganji92@gmail.com>
2022-07-28 10:36:40 +0430

    handle login modal

--- frontend/src/components/modalSignup.jsx
+++ frontend/src/components/modalSignup.jsx
@@ -1,7 +1,7 @@
 import React from "react"; 
 import { useState } from "react"; 
 
-function Modal() {
+function SignUpModal() {
   return (
 
 <div className='w-full'>
@@ -10,19 +10,19 @@ function Modal() {
         <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 rounded-lg '>
             <h2 className='text-4xl font-bold text-center  text-white'>SignUp</h2>
             <div className='flex flex-col text-gray-400 py-2'>
-                <label>name</label>
+                <label>Name</label>
                 <input className='rounded-lg bg-gray-600 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800' type="text" />
             </div>
             <div className='flex flex-col text-gray-400 py-2'>
-                <label>username</label>
+                <label>Username</label>
                 <input className='rounded-lg bg-gray-600 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800' type="text" />
             </div>
             <div className='flex flex-col text-gray-400 py-2'>
-                <label>password</label>
+                <label>Password</label>
                 <input className='rounded-lg bg-gray-600 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800' type="password" />
             </div>
             <div className='flex flex-col text-gray-400 py-2'>
-                <label>re-password</label>
+                <label>Re-Password</label>
                 <input className='rounded-lg bg-gray-600 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800' type="password" />
             </div>
             <button className='w-full my-5 py-2 bg-teal-500 text-gray font-semibold rounded-lg'>SignUp</button>
@@ -32,4 +32,4 @@ function Modal() {
 </div>
   );
 }
-export default Modal;
+export default SignUpModal;
---
+++ frontend/src/components/modalsignin.jsx
@@ -0,0 +1,28 @@
+import React from "react"; 
+import { useState } from "react"; 
+
+function SignInModal() {
+//   const [showModal, setShowModal] = React.useState(false);
+  return (
+
+<div className='w-screen'>
+    <div className='w-screen flex flex-col justify-center items-center'>
+        {/* <img className='w-8/12 h-auto' src={bookshop} alt="" /> */}
+        <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 rounded-lg '>
+            <h2 className='text-4xl font-bold text-center  text-white'>SignIn</h2>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <label>Username</label>
+                <input className='rounded-lg bg-gray-600 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800' type="text" />
+            </div>
+            <div className='flex flex-col text-gray-400 py-2'>
+                <label>Password</label>
+                <input className='rounded-lg bg-gray-600 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800' type="password" />
+            </div>
+            <button className='w-full my-5 py-2 bg-teal-500 text-gray font-semibold rounded-lg'>SignIn</button>
+        </form>
+
+</div>
+</div>
+  );
+}
+export default SignInModal;
