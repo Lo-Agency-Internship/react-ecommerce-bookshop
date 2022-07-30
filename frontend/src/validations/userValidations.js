2f64f804a0fbe6b3b22199014e6b86c32c228001
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-07-30 15:25:39 +0430

    install yup

--- frontend/src/validations/userValidations.js
+++ frontend/src/validations/userValidations.js
@@ -4,5 +4,5 @@ export const userSchema = yup.object().shape({
     name: yup.string().required(),
     username: yup.string().email().required(),
     password: yup.string().min(8).max(12).required(),
-    re_password: yup.string().oneof([yup.ref("password", null)]).required(),
+    re_password: yup.string().min(8).max(12).required(),
 });
