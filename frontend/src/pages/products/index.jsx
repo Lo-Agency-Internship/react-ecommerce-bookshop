4cf092bd158703dc71fd5eaef37df553012a9bc7
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-08-01 13:31:21 +0430

    initialChangesProducts

--- frontend/src/pages/products/index.jsx
+++ frontend/src/pages/products/index.jsx
@@ -1,14 +1,22 @@
+import Footer from "../../components/footer";
+import Navbar from "../../components/navbar";
 import Products from "../../components/products";
 import SearchBar from "../../components/searchBar";
 
 function ProductsPage() {
 	return <>
-		<div className="">
+		<div >
+			<Navbar/>
+		</div>
+		<div >
 			<SearchBar />
 		</div>
 
 		<div  className="mt-12 flex justify-center ">
 			<Products/>
+		</div>
+		<div >
+			<Footer />
 		</div>
 	</>
 }
