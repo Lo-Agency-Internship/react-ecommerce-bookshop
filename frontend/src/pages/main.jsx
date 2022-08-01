87f80b5222698db27a4c3b08b7cf9b8ab521f60e
Farzaneh <43077286+ferizana92@users.noreply.github.com>
2022-08-01 19:20:06 +0430

    debugSignUp

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -1,18 +1,19 @@
 import SignUpModal from "../components/molecules/signUpModal";
 import SignInModal from "../components/molecules/signInModal";
+import Navbar from "../components/organisms/navbar";
 import { useState } from "react";
 
 function MainPage() {
-	const [showSignUpModal] = useState(false)
-	const [showSignInModal] = useState(false)
-	
-	const [] = useState(false)
-	return (
-		<>
-			{showSignUpModal && <SignUpModal/> }
-			{showSignInModal && <SignInModal/> }
-		</>
-	)
+    const [showSignUpModal, setShowSignUpModal] = useState(false)
+    const [showSignInModal, setShowSignInModal] = useState(false)
+    return (
+        <>
+			<div><Navbar setShowSignUp={setShowSignUpModal} setShowSignIn={setShowSignInModal}/> 
+			</div>
+          
+            {showSignUpModal && <SignUpModal/> }
+            {showSignInModal && <SignInModal/> }
+         </>
+)
 }
-
 export default MainPage
